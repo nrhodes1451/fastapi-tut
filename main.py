8babe80189aa1c6e5c84b8e8a7f0e24b010a56e3
= <nrhodes1451@gmail.com>
2025-11-16 19:43:26 +0000

    path parameters

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from fastapi-tut!")
+from enum import Enum
+
+from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
